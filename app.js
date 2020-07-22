0a91b92545cce87a1f3d6dbc5253f1094a210c4f
villetuh <keymic@gmail.com>
2020-07-22 22:00:37 +0300

    Connect to MongoDB when application is started
    Application connects to MongoDB by using mongoose plugin. Config helper
    can be used to get relevant connection string from configuration file.
    Configuration file keys DB_CONNECTION_STRING and
    DB_TEST_CONNECTION_STRING are expected to be found from .env
    configuration file.

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require('express');
 require('express-async-errors');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const tournamentsRouter = require('./controllers/tournaments');
+
+const config = require('./utils/config');
 const middleware = require('./utils/middleware');
 
 const app = express();
+
+mongoose.connect(config.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify', false);
 
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
-// Add controllers
+// Controllers
 app.use('/api/tournaments', tournamentsRouter);
 
 app.use(middleware.errorHandler);
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 
+let DB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.DB_TEST_CONNECTION_STRING
+  : process.env.DB_CONNECTION_STRING;
+
 let PORT = process.env.PORT;
 
 module.exports = {
+  DB_URI,
   PORT
 };
