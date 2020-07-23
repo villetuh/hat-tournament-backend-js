ee12c368ef56b23c2c8ed44438961e7a4df1ce79
villetuh <keymic@gmail.com>
2020-07-23 18:33:26 +0300

    Add error handling for mongoose CastErrors
    Mongoose throws CastErrors if it can't correctly cast provided id
    to ObjectId. New middleware error handler catches this errors and
    sends a proper response back.

--- utils/middleware.js
+++ utils/middleware.js
@@ -3,7 +3,10 @@ require('../utils/logger');
 const errorHandler = (error, request, response, next) => {
 
   // Add custom error handlers
-
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Unsupported id format.' });
+  }
+  
   next(error);
 };
 
