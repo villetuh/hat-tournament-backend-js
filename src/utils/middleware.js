f785f6c94dc21a83e547432b868025d636671479
villetuh <keymic@gmail.com>
2020-08-24 16:54:18 +0300

    Add middleware for authenticating a request
    AuthenticateJWT middleware can be used to easily authenticate requests.
    Middleware responds with 401 or 403 status codes if needed and extracts
    user information to new user variable on request.

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,4 +1,24 @@
+const jwt = require('jsonwebtoken');
 require('../utils/logger');
+
+const authenticateJWT = (request, response, next) => {
+  const authHeader = request.get('authorization');
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.SECRET, (error, user) => {
+      if (error) {
+        return response.sendStatus(403);
+      }
+
+      request.user = user;
+      next();
+    });
+  } else {
+    response.sendStatus(401);
+  }
+};
 
 const errorHandler = (error, request, response, next) => {
 
@@ -13,5 +33,6 @@ const errorHandler = (error, request, response, next) => {
 };
 
 module.exports = {
+  authenticateJWT,
   errorHandler
 };
